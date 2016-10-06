61608a9d4cc57f7b14a620978941a495ba08b8ee
Jacob Keen <jaclkeen@gmail.com>
2016-10-06 16:31:32 -0500

    basic animal class files set up

---
+++ Animals/Animal.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ConsoleApplication {
+    class Animal {
+        public string name {get;set;}
+        public static void sleep() {
+            Console.WriteLine("Animal is now sleeping");
+        }
+    }
+}
